c8ee0ba30681c2ab7be9d65fef51e2bc4acf6743
Sven Wiltink <sven@wiltink.dev>
2021-06-04 00:27:29 +0200

    cleanup and binary format implementation

--- seek_unix.go
+++ seek_unix.go
@@ -14,68 +14,30 @@ const (
 	SEEK_HOLE = 4
 )
 
-func SeekData(file *os.File, offset int64) (off int64, err error) {
-	return unix.Seek(int(file.Fd()), offset, SEEK_DATA)
-}
-
-func SeekHole(file *os.File, offset int64) (off int64, err error) {
-	return unix.Seek(int(file.Fd()), offset, SEEK_HOLE)
-}
-
-func SeekThroughFile(file *os.File) {
-	var offset int64 = 0
+func DetectDataSection(file *os.File, offset int64) (start int64, end int64, err error) {
 	var syserr syscall.Errno
-	var totalbytes int64 = 0
 
-	defer func() {
-		log.Printf("copied %d bytes total\n", totalbytes)
-	}()
-
-	for {
-		startOfData, err := SeekData(file, offset)
-		if errors.As(err, &syserr) {
-			if syserr == syscall.ENXIO {
-				log.Printf("seek to data returned ENXIO. End of file? Current offset: %d\n", offset)
-				return
-			}
-			log.Fatalf("error seeking to data: %d, %s", syserr, syserr)
-		} else if err != nil {
-			log.Fatalf("error seeking to data: %s", err)
+	log.Println("seeking to data. current offset ", offset)
+	startOfData, err := unix.Seek(int(file.Fd()), offset, SEEK_DATA)
+	if errors.As(err, &syserr) {
+		if syserr == syscall.ENXIO {
+			return -1, -1, io.EOF
 		}
-
-		log.Printf("offset %d start of data\n", startOfData)
-
-		endOfData, err := SeekHole(file, startOfData)
-		if errors.As(err, &syserr) {
-			if syserr == syscall.ENXIO {
-				log.Printf("seek to hole returned ENXIO. End of file?\nCurrent offset: %d", offset)
-				return
-			}
-			log.Fatalf("error seeking to hole: %d, %s", syserr, syserr)
-		} else if err != nil {
-			log.Fatalf("error seeking to hole: %s", err)
-		}
-
-		log.Printf("offset %d end of data\n", endOfData)
-
-		secionSize := endOfData - startOfData
-		log.Printf("section size: %d", secionSize)
-
-		_, err = file.Seek(startOfData, io.SeekStart)
-		if err != nil {
-			log.Fatalf("error seeking to start of section")
-		}
-
-		sectionReader := io.LimitReader(file, secionSize)
-		copied, err := io.Copy(os.Stdout, sectionReader)
-		if err != nil {
-			log.Fatalf("error copying data: %s", err)
-		}
-
-		totalbytes += copied
-
-		log.Printf("copied %d bytes", copied)
-
-		offset = endOfData
+		return -1, -1, err
 	}
+
+	if err != nil {
+		return -1, -1, err
+	}
+
+	log.Println("seeking to hole. current offset ", offset)
+	endOfData, err := unix.Seek(int(file.Fd()), startOfData, SEEK_HOLE)
+	if errors.As(err, &syserr) {
+		if syserr == syscall.ENXIO {
+			return -1, -1, io.EOF
+		}
+		return -1, -1, err
+	}
+
+	return startOfData, endOfData, err
 }
--- sparsecat.go
+++ sparsecat.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/binary"
+	"errors"
 	"flag"
+	"io"
 	"log"
 	"os"
 )
@@ -15,5 +18,78 @@ func main() {
 		log.Fatalf("unable to open file: %s", err)
 	}
 
-	SeekThroughFile(file)
+	var offset int64 = 0
+
+	info, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("file size: %d", info.Size())
+
+	err = writeSizeSection(os.Stdout, info.Size())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		start, end, err := DetectDataSection(file, offset)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = writeDataSection(os.Stdout, file, start, end)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		offset = end
+	}
+
+	err = writeEndSection(os.Stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func writeEndSection(w io.Writer) error {
+	_, err := w.Write([]byte("e"))
+	return err
+}
+
+func writeSizeSection(w io.Writer, size int64) error {
+	buf := make([]byte, 9)
+	buf[0] = 's'
+
+	binary.LittleEndian.PutUint64(buf[1:], uint64(size))
+
+	_, err := w.Write(buf)
+	return err
+}
+
+func writeDataSection(w io.Writer, r io.ReadSeeker, start int64, end int64) error {
+	_, err := r.Seek(start, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, 1+8+8)
+	buf[0] = 'w'
+
+	length := end - start
+
+	binary.LittleEndian.PutUint64(buf[1:], uint64(start))
+	binary.LittleEndian.PutUint64(buf[1+8:], uint64(length))
+
+	_, err = w.Write(buf)
+	if err != nil {
+		return nil
+	}
+
+	_, err = io.Copy(w, io.LimitReader(r, length))
+	return err
 }
